8f7309fd054ef472984bf6728d330a9bc85c8e5f
JadeFox417 <tepanuspardosi.jp@gmail.com>
2022-03-29 22:18:59 +0800

    first commit

---
+++ resources/views/dashboard/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+	<div class="pull-right hidden-xs">
+		<b>Version</b> 1.0.0
+	</div>
+	<strong>Copyright &copy; 2017 <a href="https://facebook.com/caesaralilamondo" target="_blank">KATERING APP</a>.</strong> in Makassar, Indonesia. | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+	
+</footer>
