b6306381ceb0047d87feef137280b936ad1d93b9
farahdq <73812417+farahdq@users.noreply.github.com>
2022-06-07 22:04:01 +0700

    Update view.php

--- config/view.php
+++ config/view.php
@@ -24,8 +24,7 @@ return [
     |
     | This option determines where all the compiled Blade templates will be
     | stored for your application. Typically, this is within the storage
-    | directory. However, as usual, you are free to change this value.
-    |
+    | directory.
     */
 
     'compiled' => realpath(storage_path('framework/views')),
